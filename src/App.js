7a3be7851a4c8ac31b843d36339ac0c399b7fd40
Septian A Tama <sfirst.tama@gmail.com>
2018-03-08 14:26:27 +0700

    installing mobx-react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import CounterStore from './store'
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+@observer class App extends Component {
   render() {
+    const store = CounterStore
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h1>{ store.count }</h1>
+        </div>
       </div>
     );
   }
